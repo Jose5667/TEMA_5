4e5f2c69a7afb3bcff70850d9aaccd2fb8e86c94
Jose <jarios@ceste.com>
2022-10-28 23:12:13 +0200

    Ejercicio 1

--- app/src/main/java/com/example/tema_5/MainActivity.java
+++ app/src/main/java/com/example/tema_5/MainActivity.java
@@ -2,13 +2,35 @@ package com.example.tema_5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(new Dibujar(this));
+    }
+}
+
+class Dibujar extends View{
+    Paint pincel = new Paint();
+    public Dibujar(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        pincel.setColor(Color.BLUE);
+        pincel.setStrokeWidth(10);
+        pincel.setStyle(Paint.Style.STROKE);
+        canvas.drawCircle(300,300,90,pincel);
+
     }
 }
